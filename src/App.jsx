8c890d84c6769a3b4968f5895152d893e7661d18
unknown <mehadyhasan1000@gmail.com>
2023-08-12 21:00:02 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
-
-
 const App = () => {
   return (
     <div>
-      <h1>fkg,kiu</h1>
+<button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+
     </div>
   )
 }
