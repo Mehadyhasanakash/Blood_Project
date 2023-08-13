ca9a2fdaa1d4a2108ba7f5acce37cea04bef6fa3
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2023-08-13 21:42:47 +0600

    data_fetch

---
+++ src/compronents/PopulerMenu.jsx
@@ -0,0 +1,11 @@
+
+const PopulerMenu = () => {
+  return (
+    <>
+    <h1>Populer_Menu</h1>
+
+    </>
+  )
+}
+
+export default PopulerMenu
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react"
+import { useEffect } from "react"
 import Banner from "../../compronents/Banner"
 import BloodCetagory from "../../compronents/BloodCetagory"
+import PopulerMenu from "../../compronents/PopulerMenu"
 
 const Home = () => {
+
+  const[menu, setMenu] = useState([]);
+
+
+  useEffect(()=>{
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => {
+      const populerData = data.products.filter(item => item.brand ==="Apple")
+      console.log(populerData)
+    } )
+  }, [])
   return (
     <div>
         <Banner/>
         <BloodCetagory/>
+        <PopulerMenu/>
     </div>
   )
 }
