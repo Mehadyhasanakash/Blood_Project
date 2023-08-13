922b56f7f4f6f63d20ddcc8ff51de1a30349be94
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2023-08-13 21:43:59 +0600

    setUp

--- src/compronents/PopulerMenu.jsx
+++ src/compronents/PopulerMenu.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const PopulerMenu = () => {
+    const[menu, setMenu] = useState([]);
+
+
+  useEffect(()=>{
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => {
+      const populerData = data.products.filter(item => item.brand ==="Apple")
+      setMenu(populerData)
+    } )
+  }, [])
+
+
+  
   return (
     <>
     <h1>Populer_Menu</h1>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,10 @@
-import { useState } from "react"
-import { useEffect } from "react"
 import Banner from "../../compronents/Banner"
 import BloodCetagory from "../../compronents/BloodCetagory"
 import PopulerMenu from "../../compronents/PopulerMenu"
 
 const Home = () => {
 
-  const[menu, setMenu] = useState([]);
-
-
-  useEffect(()=>{
-    fetch('menu.json')
-    .then(res => res.json())
-    .then(data => {
-      const populerData = data.products.filter(item => item.brand ==="Apple")
-      console.log(populerData)
-    } )
-  }, [])
+  
   return (
     <div>
         <Banner/>
